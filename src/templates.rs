fa2db9bf362e9b0d281b10610e234ce3504d21c5
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-14 13:28:43 -0500

    feat: create template in rust

---
+++ src/templates.rs
@@ -0,0 +1,29 @@
+use askama::Template;
+
+pub(crate) struct Bookmark {
+	// icon: String,
+	id: String,
+	link: String,
+	name: String,
+	shortcut: Option<String>,
+}
+
+impl Bookmark {
+	pub(crate) fn new(link: String, name: String, shortcut: Option<String>) -> Self {
+		// let id = format!("bookmark-{}", foo); // Do something to get number in list of bookmarks
+		let id = String::from("foo");
+
+		Self {
+			id,
+			link,
+			name,
+			shortcut
+		}
+	}
+}
+
+#[derive(Template)]
+#[template(path = "home.html")]
+pub(crate) struct HomePage {
+	pub(crate) bookmarks: Vec<Bookmark>,
+}
