17962de9ecdad100b66fe0885115e2c744369a1f
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-14 17:54:06 -0500

    style: fix caps for Homepage

--- src/templates.rs
+++ src/templates.rs
@@ -4,7 +4,7 @@ use askama::Template;
 
 #[derive(Template)]
 #[template(path = "home.html")]
-pub(crate) struct HomePage {
+pub(crate) struct Homepage {
 	pub(crate) title: Option<String>,
 	pub(crate) bookmarks: Vec<Bookmark>,
 }
