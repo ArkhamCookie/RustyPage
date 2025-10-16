959e297f65887d5c4167af18db4993425a3b33f1
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-15 19:05:09 -0500

    feat: impl new and render for template

--- src/templates.rs
+++ src/templates.rs
@@ -1,4 +1,4 @@
-use crate::config::ParsedBookmark;
+use crate::config::{Config, ParsedBookmark};
 
 use askama::Template;
 
@@ -9,4 +9,19 @@ use serde::Deserialize;
 pub(crate) struct Homepage {
 	pub(crate) title: Option<String>,
 	pub(crate) bookmarks: Vec<ParsedBookmark>,
+}
+
+impl Homepage {
+	pub(crate) fn new(config: &Config) -> Self {
+		let converted_bookmarks = ParsedBookmark::convert_all(&config.bookmarks);
+
+		Self {
+			title: config.title.clone(),
+			bookmarks: converted_bookmarks,
+		}
+	}
+
+	pub(crate) fn render(template: &Self) -> Result<String, askama::Error> {
+		template.render()
+	}
 }
