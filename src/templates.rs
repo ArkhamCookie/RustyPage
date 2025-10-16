f32ac71f84dac8a37bff3fd63e62385dca6bdfb3
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-15 19:03:40 -0500

    fix: fix imports and type

--- src/templates.rs
+++ src/templates.rs
@@ -1,10 +1,12 @@
-use crate::config::Bookmark;
+use crate::config::ParsedBookmark;
 
 use askama::Template;
 
-#[derive(Template)]
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Template)]
 #[template(path = "home.html")]
 pub(crate) struct Homepage {
 	pub(crate) title: Option<String>,
-	pub(crate) bookmarks: Vec<Bookmark>,
+	pub(crate) bookmarks: Vec<ParsedBookmark>,
 }
