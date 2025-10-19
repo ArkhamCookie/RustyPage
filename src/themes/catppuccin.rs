ed70bad2e5242635e5a66f80fb52d30c01737a15
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-19 09:41:35 -0500

    feat: create themes section

---
+++ src/themes/catppuccin.rs
@@ -0,0 +1,8 @@
+pub(crate) const CATPPUCCIN_FRAPPE: &'static str = "<style>
+		:root {
+			--external-background: #303446;
+			--internal-background: #292C3C;
+			--foreground: #C6D0F5;
+			--comment: #B5BFE2
+		}
+	</style>";
