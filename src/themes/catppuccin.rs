d4c1464b189b3de2dcb6880cf7af0de5415c534c
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-22 09:18:56 -0500

    feat: add surface-zero color to themes

--- src/themes/catppuccin.rs
+++ src/themes/catppuccin.rs
@@ -6,6 +6,7 @@ pub(crate) const CATPPUCCIN_LATTE: &str = "<style>
 			--base: #EFF1F5;
 			--text: #4C4F69;
 			--link: #1E66F5;
+			--surface-zero: #CCD0DA
 		}
 	</style>";
 
@@ -14,6 +15,7 @@ pub(crate) const CATPPUCCIN_FRAPPE: &str = "<style>
 			--base: #303446;
 			--text: #C6D0F5;
 			--link: #8CAAEE;
+			--surface-zero: #414559;
 		}
 	</style>";
 
@@ -22,6 +24,7 @@ pub(crate) const CATPPUCCIN_MACCHIATO: &str = "<style>
 			--base: #24273A;
 			--text: #CAD3F5;
 			--link: #8ADDF4;
+			--surface-zero: #363A4F
 		}
 	</style>";
 
@@ -30,5 +33,6 @@ pub(crate) const CATPPUCCIN_MOCHA: &str = "<style>
 			--base: #1E1E2E;
 			--text: #CDD6F4;
 			--link: #89B4FA;
+			--surface-zero: #313244;
 		}
 	</style>";
