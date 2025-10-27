b7f767a8716f4f16b9ca664ca072768550c6c82b
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-27 11:20:30 -0500

    feat: add in base dracula theme

--- src/templates.rs
+++ src/templates.rs
@@ -2,6 +2,7 @@ use crate::config::{Config, ParsedBookmark};
 use crate::themes::catppuccin::{
 	CATPPUCCIN_FRAPPE, CATPPUCCIN_LATTE, CATPPUCCIN_MACCHIATO, CATPPUCCIN_MOCHA,
 };
+use crate::themes::dracula::DRACULA_DEFAULT;
 
 use askama::Template;
 
@@ -28,6 +29,7 @@ impl Homepage {
 				"catppuccin" | "catppuccin-frappe" | "catppuccin_frappe" | "frappe" => &CATPPUCCIN_FRAPPE,
 				"catppuccin-macchiato" | "catppuccin_macchiato" | "macchiato" => &CATPPUCCIN_MACCHIATO,
 				"catppuccin-mocha" | "catppuccin_mocha" | "mocha" => &CATPPUCCIN_MOCHA,
+				"dracula" => &DRACULA_DEFAULT,
 				"" => "",
 				_ => {
 					eprintln!("WARNING: Unreconized theme in config file!");
