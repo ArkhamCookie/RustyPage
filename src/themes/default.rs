2acb36ef1abea703d1eef18e426df836ea5ed145
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-27 12:07:55 -0500

    feat: add default theme

--- src/themes/default.rs
+++ src/themes/default.rs
@@ -1,1 +1,12 @@
 //! Default theme if no theme is selected
+
+/// Default theme for RustyPage
+pub(crate) const DEFAULT: &str = "<style>
+		:root {
+			--background: #FFFFFF;
+			--text: #000000;
+			--link: #0000EE;
+			--surface-zero: #FFFFFF;
+			--border: #000000;
+		}
+	</style>";
