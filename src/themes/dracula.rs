81ff700c1f43fc29f2b54a45f388313ff3737f5d
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-27 11:08:22 -0500

    feat: create dracula theme
    best guess of what it should look like (as dracula is meant for editors by default)

--- src/themes/dracula.rs
+++ src/themes/dracula.rs
@@ -1,1 +1,12 @@
 //! [Dracula](https://draculatheme.com) Themes
+
+/// [Dracula](https://draculatheme.com) Main Theme
+pub(crate) const DRACULA_DEFAULT: &str = "<style>
+		:root {
+			--background: #282A36;
+			--text: #F8F8F2;
+			--link: #8BE9FD;
+			--surface-zero: #6272A4
+			--border: #44475A;
+		}
+	<style>";
