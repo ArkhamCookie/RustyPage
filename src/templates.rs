6304e160fd3f4efde77abf4819213a3e70a5e285
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-10-30 13:49:43 -0500

    feat: add support for favicons through urls

--- src/templates.rs
+++ src/templates.rs
@@ -14,7 +14,7 @@ use serde::Deserialize;
 #[template(path = "home.html", escape = "none")]
 pub(crate) struct Homepage {
 	pub(crate) title: Option<String>,
-	pub(crate) favicon: Option<String>,
+	pub(crate) favicon: String,
 	pub(crate) theme: &'static str,
 	pub(crate) clock: Option<Clock>,
 	pub(crate) search_engine: Option<String>,
@@ -25,6 +25,17 @@ pub(crate) struct Homepage {
 impl Homepage {
 	/// Create Homepage with a given config
 	pub(crate) fn new(config: &Config) -> Self {
+		let mut favicon = String::new();
+		if let Some(favicon_input) = &config.favicon {
+			let filtered_favicon = &favicon_input.as_str();
+
+			if filtered_favicon.starts_with("http") {
+				favicon = format!("<link href=\"{}\" rel=\"icon\" type=\"image/x-icon\">", favicon_input);
+			} else {
+				favicon = format!("<link href=\"data:image/x-icon;base64,{}\" rel=\"icon\" type=\"image/x-icon\">", favicon_input);
+			}
+		}
+
 		let theme = match &config.theme {
 			Some(theme_name) => match theme_name.to_ascii_lowercase().as_str() {
 				"catppuccin-latte" | "catppuccin_latte" | "latte" => &CATPPUCCIN_LATTE,
@@ -60,7 +71,7 @@ impl Homepage {
 
 		Self {
 			title: config.title.clone(),
-			favicon: config.favicon.clone(),
+			favicon,
 			theme,
 			clock: config.clock.clone(),
 			search_engine: config.search_engine.clone(),
